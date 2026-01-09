c2f50d86f0b7b6501bece274747af4917d3b7802
samithspalan <samithpoojary12345@gmail.com>
2026-01-09 18:43:23 +0530

    fix express 5 spa routing

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ app.use("/api/messages", messageroutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
- app.get("/*", (req, res) => {
+ app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
 
 }
 
