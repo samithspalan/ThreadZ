ba85c8f0222903403abc6b5a91629a00d477ac58
samithspalan <samithpoojary12345@gmail.com>
2026-01-09 19:06:46 +0530

    fix express 5 spa routing

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageroutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
- app.get(/^(?!\/api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+//  app.get(/^(?!\/api).*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
 
 
-}
+// }
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
