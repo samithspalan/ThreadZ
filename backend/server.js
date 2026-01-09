f04ad42aaa177cab079495d85c01536e71328412
samithspalan <samithpoojary12345@gmail.com>
2026-01-09 18:36:16 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ app.use("/api/messages", messageroutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+ app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 }
 
 server.listen(PORT, () => {
